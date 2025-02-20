5112a26eac00cf62e37c19bf32296958fb049863
LeeJinHyuk9358 <leejh9358@naver.com>
2025-02-20 21:39:27 +0900

    feat: 20250220 iphone_memo_app

--- chapter14_iphone_memo/script/list_service.js
+++ chapter14_iphone_memo/script/list_service.js
@@ -3,17 +3,17 @@ function loadList() {
   ? JSON.parse(localStorage.getItem("boardDatas"))
   : [];
 
-  const boardList = document.querySelector('board-list-container'); 
-    boardList.innerHTML = boardDatas.map(data => `
-      <li class="board-box">
-        <header class="board-header">
-          <h1>${data.title}(${data.writer})</h1>
-        </header>
-        <main class="board-main">
+  const boardList = document.querySelector('.board-list-container');
+  boardList.innerHTML = boardDatas.map(data => `
+    <li class="board-box">
+      <header class="board-header">
+        <h1>${data.title}(${data.writer})</h1>
+      </header>
+      <main class="board-main">
         <pre>${data.content}</pre>
-        </main>
-      </li>
-      `).join("");
+      </main>
+    </li>
+    `).join("");
 }
 
 loadList();
